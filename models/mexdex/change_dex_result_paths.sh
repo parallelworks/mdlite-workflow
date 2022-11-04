83e8f1a112be7367d25d6f9ba3e6df7b56053a3f
Parallel Works app-run user <sfgary@mgmt-sfgary-gcev2-00022.c.modular-magpie-167320.internal>
2022-11-04 14:20:58 +0000

    First steps in DEX paths adjuster

---
+++ models/mexdex/change_dex_result_paths.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#=================================
+# Design Explorer (DEX) must have
+# absolute paths to work. So, in 
+# order to move DEX data from 
+# place to place, the paths need
+# to be changed in the *.html and
+# *.csv file associated with the
+# data set.
+#
+# This script will modify the paths
+# in the *.html and *.csv files.
+# The input is:
+# $1 = basename of the *.html and 
+#      *.csv files.
+# $2 = the original (current) path
+# $3 = what the path should be changed
+#      to with a direct sed search
+#      and replace.
+#
+# $2 and $3 should be used as 
+# dirnames; i.e. the prefix of the
+# path that needs to change.
+#
+# For example, if the original paths are:
+# /home/user/mdlite-workflow
+# and we want to change them to
+# /pw/clusters/pool/mdlite-workflow
+# then the invocation is:
+#
+# change_dex_result_paths.sh mdlite_dex /home/user/ /pw/clusters/pool/
+#================================
+
+# CLI inputs
+dex_bname=$1
+dex_path_1=$2
+dex_path_2=$3
+
+# Filter for sed
+dex_path_1_for_sed=$(echo $dex_path_1 | sed 's/\//\\\//g')
+
+# Cross check
+echo Working on ${dex_bname}.html and ${dex_bname}.csv
+echo $dex_path_1_for_sed
