6c8b517b225947031d17a71891e47ece58737241
StefanGary <sfgary@parallelworks.com>
2023-01-23 11:07:04 +0000

    First draft of NO-OP workflow
    This is setup so we can check the parsl_utils infrastructure
    and staging before any command execution. Also, added checking
    for and using default executors.json and local.conf if missing.

--- workflow_form_launcher.sh
+++ workflow_form_launcher.sh
@@ -7,6 +7,17 @@ git clone -b dev https://github.com/parallelworks/parsl_utils.git parsl_utils
 # Convert Jupyter notebook to main.py
 jupyter nbconvert --log-level 0 --to script --output main.py main.ipynb
 
+# If not present, use default local.conf and executors.json
+if [ -f "local.conf" ]; then
+    echo Using default local.conf...
+    cp local.conf.example local.conf
+fi
+
+if [ -f "executors.json" ]; then
+    echo Using default executors.json...
+    cp executors.json.example executors.json
+fi
+
 # Cannot run scripts inside parsl_utils directly
 bash parsl_utils/main.sh $@
 
