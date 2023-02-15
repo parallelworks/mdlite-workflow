b2bafb255983576e131c4b1f01616d4ddba49bd4
StefanGary <sfgary@parallelworks.com>
2023-02-15 14:31:13 +0000

    Fixed bug with MD runner wrapper
    Each invocation of the md binary from runMD.sh was attempting
    to run in the same directory.  This meant that runMD.sh was
    routinely deleting/overwriting some of the output of the md
    binary and then metrics for DesignExplorer were lost.

--- models/mdlite/runMD.sh
+++ models/mdlite/runMD.sh
@@ -6,26 +6,45 @@ caseParams=$1
 metricout=$HOME/$2
 trjout=$HOME/$3
 
-echo $caseParams
-echo $metricout
-echo $trjout
+# Check command line inputs
+echo "======================="
+echo caseParams: $caseParams
+echo metricout: $metricout
+echo trajout: $trjout
+echo "======================"
+echo " "
 
 npart=$(echo $caseParams | tr '|' '\n' | grep npart | awk -F',' '{print $2}')
 steps=$(echo $caseParams | tr '|' '\n' | grep steps | awk -F',' '{print $2}')
 trsnaps=$(echo $caseParams | tr '|' '\n' | grep trsnaps | awk -F',' '{print $2}')
 mass=$(echo $caseParams | tr '|' '\n' | grep mass | awk -F',' '{print $2}')
 
-cd ${0%/*} || exit 1
+# Check parsing of caseParams
+echo " "
+echo "========================"
+echo Number of particles: $npart
+echo Simulation steps: $steps
+echo Visulation snaps: $trsnaps
+echo Particle mass: $mass
+echo "======================="
+echo " "
 
-chmod 777 * -R
+# Get directory with binary. Use $0, but
+# %/* will match the last instance of "/"
+# in $0 and remove everything after it.
+bindir=${0%/*}
 
-./md 2 $npart $steps $trsnaps ".0001" $mass "0.1 1.0 0.2 0.05 50.0 0.1" 2.5 2.0 $RANDOM md.out $trjout
+# Not needed since permissions are preserved.
+#chmod 777 * -R
 
-# extract the metrics
+${bindir}/md 2 $npart $steps $trsnaps ".0001" $mass "0.1 1.0 0.2 0.05 50.0 0.1" 2.5 2.0 $RANDOM md.out $trjout
+
+# Extract the metrics
 echo $trsnaps > $metricout
 cat md.out | tail -1 | awk '{print $2, $3, $4}' | tr ' ' '\n' >> $metricout
 
-rm md.out
+# Clean up
+#rm md.out
 
 exit 0
 
